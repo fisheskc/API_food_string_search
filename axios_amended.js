0a3d2979008f9dcefa9cb68ed2bb6cbc170e4291
Karen Collins <fisheskc@users.noreply.github.com>
2021-10-21 20:47:47 +0100

    Create axios_amended.js

---
+++ axios_amended.js
@@ -0,0 +1,19 @@
+let searchMenu = document.forms['search-menu'].querySelector('input')
+let div = document.createElement('div');
+
+    searchMenu.addEventListener("keyup", async function(e) {
+    e.preventDefault();
+    const term = e.target.value.toLowerCase();
+
+    axios.get("https://www.themealdb.com/api/json/v1/1/filter.php?i=")
+        .then(res => {
+        let apiRoute = res.data.meals
+            apiRoute.map((item) => {
+           if(item.strMeal.toLowerCase().indexOf(term) != -1) {
+            div.innerHTML = `<h2>This is the results of your search: ${item.strMeal}</h2>`
+            return item.strMeal; 
+           }
+        })
+    })
+        document.body.appendChild(div) 
+    })
